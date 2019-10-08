11ce19df1cdc0c5c775e78ecdcdbaa9ea1b7aec5
learn <learn@learn08.local>
2019-10-08 14:28:16 -0700

    piglatin

--- src/App.js
+++ src/App.js
@@ -9,14 +9,44 @@ class App extends React.Component {
       phraseTranslated: 'This is where your translated sentence will appear.'
     }
   }
-  
+
   // The translate function is where you will put your logic to convert the sentence entered by the user to pig location.  What is currently in the function will only directly copy what the user has entered.
 
   translate = (e) => {
     e.preventDefault()
+    // turn the input string into an array
     let translated = this.state.phrase
+
+    let transNew = translated.split(" ")
+
+    let runEverything = function(array) {
+        for (let i = 0; i < array.length; i++) {
+        //    console.log(array[i] + "way")
+                if (array[i].indexOf('a') === 0 || array[i].indexOf('e') === 0 || array[i].indexOf('i') === 0 || array[i].indexOf('o') === 0 || array[i].indexOf('u') === 0) {
+                    let letters = array[i].split("")
+                    var lettersplit = letters.join("") + "way"
+                    console.log(lettersplit)
+                     return lettersplit
+                
+                }
+                else{
+                    console.log("error")
+                }
+
+        }
+    }
+        // for (let i = 0; i < array.length; i ++) {
+            // if (array[i].charAt(0) === 'a' && array[i].charAt(0) === 'e' && array[i].charAt(0) === 'i' && array[i].charAt(0) === 'o' && array[i].charAt(0) === 'u') {
+        //         let letters = array.split("")
+        //         var lettersplit = letters.join(" ") + "way"
+        //         console.log(lettersplit)
+        //          return lettersplit
+        //     }
+        // }
+    runEverything(transNew)
+// translated = lettersplit
     this.setState({phraseTranslated: translated})
-  }
+}
 
   handleChange = (e) => {
     this.setState({phrase: e.target.value})
@@ -24,7 +54,7 @@ class App extends React.Component {
 
   render() {
     return (
-      
+
       <div className="wrapper">
         <header className="box header">
           <div id="pigImage">
@@ -36,7 +66,7 @@ class App extends React.Component {
             <form className="info" onSubmit={this.translate}>
               <label htmlFor="input-phrase">Translate this: </label>
               <input name="input-phrase" onChange={this.handleChange}></input>
-              <input className="button" type="submit" value="Submit" />
+              <input className="button" type="submit" onClick={this.translate} value="Submit" />
             </form>
           </div>
         </sidebar>
